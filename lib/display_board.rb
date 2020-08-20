58d384196ed90945bcfba62f38e9d5cf2b44b6e9
Brandon Thomas <brandonvthomas97@gmail.com>
2020-08-20 16:19:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["0","1","2","3","4","5","6","7","8"]
+board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   puts "   |   |   "
@@ -8,7 +8,6 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  puts "X:#{board[5]}"
 end
 
 display_board(board)
